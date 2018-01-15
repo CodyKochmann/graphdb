522d24b04e466df5e240986b2e9659a9ca535f17
Cody Kochmann <kochmanncody@gmail.com>
2018-01-15 11:55:37 -0500

    switched the serializer to use dill for less security issues and more supported types

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'graphdb',
   packages = ['graphdb'], # this must be the same as the name above
-  version = '2017.11.21',
+  version = '2018.1.18',
   install_requires = ["generators", "dill"],
   description = 'sqlite based graph database for storing native python objects and their relationships to each other',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/graphdb',
-  download_url = 'https://github.com/CodyKochmann/generators/tarball/2017.11.21',
+  download_url = 'https://github.com/CodyKochmann/generators/tarball/2018.1.18',
   keywords = ['graphdb', 'graph', 'sqlite', 'database', 'db', 'node', 'relation', 'attribute'],
   classifiers = [],
 )
