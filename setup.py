b36f0a919d46b59cbf6a8ed92689f62966854e90
Cody Kochmann <kochmanncody@gmail.com>
2018-03-17 18:07:20 -0400

    graphdb is now thread safe thanks to new state machine internals

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from distutils.core import setup
 
+version = '2018.3.17'
+
 setup(
   name = 'graphdb',
   packages = ['graphdb'], # this must be the same as the name above
-  version = '2018.1.18',
+  version = version,
   install_requires = ["generators", "dill"],
   description = 'sqlite based graph database for storing native python objects and their relationships to each other',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/graphdb',
-  download_url = 'https://github.com/CodyKochmann/generators/tarball/2018.1.18',
+  download_url = 'https://github.com/CodyKochmann/generators/tarball/{}'.format(version),
   keywords = ['graphdb', 'graph', 'sqlite', 'database', 'db', 'node', 'relation', 'attribute'],
   classifiers = [],
 )
