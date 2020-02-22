95ea1dbd894f483ba269b90945bf5ea69c005934
Cody Kochmann <kochmanncody@gmail.com>
2020-02-22 12:27:36 -0700

    fixed version check in test generation

--- graphdb/tests/__init__.py
+++ graphdb/tests/__init__.py
@@ -9,6 +9,6 @@ __all__ = ['TestGraphDB', 'TestSQLiteGraphDB']
 TestGraphDB       = generate_api_tests(GraphDB)
 TestSQLiteGraphDB = generate_api_tests(SQLiteGraphDB)
 
-if sys.version_info <= (3, 6):
+if sys.version_info >= (3, 6):
 	__all__.append('TestRamGraphDB')
 	TestRamGraphDB = generate_api_tests(RamGraphDB)
