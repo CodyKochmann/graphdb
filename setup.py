b5460b3fc0ee7ea2685670f10e558a7ee0de3fc3
Cody Kochmann <kochmanncody@gmail.com>
2020-02-22 09:56:29 -0700

    reworked setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,40 @@
 from distutils.core import setup
+import sys, os, setuptools
 
 version = '2020.2.4'
+name = 'graphdb'
+description = 'sqlite based graph database for storing native python objects and their relationships to each other',
+keywords = ['graphdb', 'graph', 'sqlite', 'database', 'db', 'node', 'relation', 'attribute'],
+
+packages = setuptools.find_packages()
+
+assert name in packages, [name, packages]  # if package name doesnt show up, something is wrong
+
+def using_ios_stash():
+    ''' returns true if sys path hints the install is running on ios '''
+    print('detected install path:')
+    print(os.path.dirname(__file__))
+    module_names = set(sys.modules.keys())
+    return 'stash' in module_names or 'stash.system' in module_names
+
+def requires():
+    ''' generates the package requirements live based on system configuration '''
+    yield "generators"
+    yield "strict_functions"
+    if not using_ios_stash():
+        yield "dill"
 
 setup(
-  name = 'graphdb',
-  packages = ['graphdb'], # this must be the same as the name above
+  name = name,
   version = version,
-  install_requires = ["generators", "strict_functions", "dill"],
-  description = 'sqlite based graph database for storing native python objects and their relationships to each other',
+  packages = packages,
+  install_requires = list(requires()),
+  zip_safe=True,
+  description = description,
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
-  url = 'https://github.com/CodyKochmann/graphdb',
-  download_url = 'https://github.com/CodyKochmann/generators/tarball/{}'.format(version),
-  keywords = ['graphdb', 'graph', 'sqlite', 'database', 'db', 'node', 'relation', 'attribute'],
-  classifiers = [],
+  url = 'https://github.com/CodyKochmann/{}'.format(name),
+  download_url = 'https://github.com/CodyKochmann/{}/tarball/{}'.format(name, version),
+  keywords = keywords,
+  classifiers = []
 )
