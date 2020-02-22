cef4df5c69d44bc52a14d51701a592c8c0fae7f4
Cody Kochmann <kochmanncody@gmail.com>
2020-02-22 12:21:43 -0700

    switched order of lines

--- graphdb/tests/__init__.py
+++ graphdb/tests/__init__.py
@@ -10,5 +10,5 @@ TestGraphDB       = generate_api_tests(GraphDB)
 TestSQLiteGraphDB = generate_api_tests(SQLiteGraphDB)
 
 if sys.version_info <= (3, 6):
-	TestRamGraphDB    = generate_api_tests(RamGraphDB)
 	__all__.append('TestRamGraphDB')
+	TestRamGraphDB    = generate_api_tests(RamGraphDB)
